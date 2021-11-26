627785396f396b197d1068f17be486cc8d80e3ed
Chad Scarborough <chadscarborough4@gmail.com>
2021-11-26 13:41:05 -0800

    Created new classes for storing lists of Yaku objects

---
+++ Yaku/YakuLists/YakuList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RMU.Yaku.YakuLists
+{
+    public static class YakuList
+    {
+        private static List<AbstractYaku> _yakuList = new List<AbstractYaku>
+        {
+            new AllSimples(),
+            new AllTerminalsAndHonors(),
+            new GreenDragon(),
+            new RedDragon(),
+            new WhiteDragon()
+        };
+
+        public static List<AbstractYaku> GetYakuList()
+        {
+            return _yakuList;
+        }
+    }
+}
---
+++ Yaku/YakuLists/YakumanList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RMU.Yaku.Yakuman;
+
+namespace RMU.Yaku.YakuLists
+{
+    public static class YakumanList
+    {
+        private static List<AbstractYakuman> _yakumanList = new List<AbstractYakuman>
+        {
+            new AllHonors(),
+            new AllTerminals(),
+            new BigThreeDragons(),
+            new FourBigWinds()
+        };
+    }
+}
