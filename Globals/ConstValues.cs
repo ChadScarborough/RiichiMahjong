526d94c1efaff31ebd900f68a4eb28a3f8849015
Chad Scarborough <chadscarborough4@gmail.com>
2021-11-26 09:11:34 -0800

    Extracted constant values into a single class

---
+++ Globals/ConstValues.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RMU.Globals
+{
+    public static class ConstValues
+    {
+        public const int GREEN_DRAGON = 1;
+        public const int RED_DRAGON = 2;
+        public const int WHITE_DRAGON = 3;
+
+        public const int EAST_WIND = 1;
+        public const int SOUTH_WIND = 2;
+        public const int WEST_WIND = 3;
+        public const int NORTH_WIND = 4;
+
+        public const int NUMBER_OF_TILES_IN_FULL_HAND = 14;
+    }
+}
