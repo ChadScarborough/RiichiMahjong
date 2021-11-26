9d8bc8cb9e12f133135e338273a07cb4824c9471
Chad Scarborough <chadscarborough4@gmail.com>
2021-11-26 10:26:05 -0800

    Added All Simples

---
+++ Yaku/AllSimples.cs
@@ -0,0 +1,46 @@
+﻿using RMU.Hand;
+using RMU.Tiles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RMU.Yaku
+{
+    public class AllSimples : Yaku
+    {
+        private readonly string _name;
+        private readonly int _value;
+        private IGetNameBehaviour getNameBehaviour;
+        private IGetValueBehaviour getValueBehaviour;
+
+        public AllSimples()
+        {
+            _name = "All Simples";
+            _value = 1;
+            getNameBehaviour = new StandardGetNameBehaviour();
+            getValueBehaviour = new StandardGetValueBehaviour();
+        }
+
+        public override bool CheckYaku(IHand hand, TileObject extraTile)
+        {
+            foreach (TileObject tile in hand.Listify(extraTile))
+            {
+                if (tile.IsHonor() || tile.IsTerminal())
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public override string GetName()
+        {
+            return getNameBehaviour.GetName(_name);
+        }
+
+        public override int GetValue()
+        {
+            return getValueBehaviour.GetValue(_value);
+        }
+    }
+}
