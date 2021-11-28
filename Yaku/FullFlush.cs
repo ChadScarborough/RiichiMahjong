7fd3121fb213908d9e9745e80e47c451bc7a8bc2
Chad Scarborough <chadscarborough4@gmail.com>
2021-11-28 05:26:49 -0800

    Implemented Full Flush

--- Yaku/FullFlush.cs
+++ Yaku/FullFlush.cs
@@ -3,11 +3,15 @@ using RMU.Tiles;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using RMU.Globals;
 
 namespace RMU.Yaku
 {
     public class FullFlush : AbstractYaku
     {
+        private Enums.Suit _suit;
+        private List<TileObject> _tiles;
+
         public FullFlush()
         {
             _name = "Full Flush";
@@ -19,6 +23,25 @@ namespace RMU.Yaku
         public override bool CheckYaku(IHand hand, TileObject extraTile)
         {
             _hand = hand;
+            _tiles = hand.Listify(extraTile);
+            if (SuccessfullyFoundNonHonorSuit() == false) return false;
+            foreach(TileObject tile in _tiles)
+            {
+                if (tile.GetSuit() != _suit) return false;
+            }
+            return true;
+        }
+
+        private bool SuccessfullyFoundNonHonorSuit()
+        {
+            foreach(TileObject tile in _tiles)
+            {
+                if(tile.IsHonor() == false)
+                {
+                    _suit = tile.GetSuit();
+                    return true;
+                }
+            }
             return false;
         }
     }
