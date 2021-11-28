2b122ac010df5ea7b25953f8bbe8a6f6944f43fa
Chad Scarborough <chadscarborough4@gmail.com>
2021-11-28 04:59:02 -0800

    Began implementing full flush

---
+++ Yaku/FullFlush.cs
@@ -0,0 +1,25 @@
+﻿using RMU.Hand;
+using RMU.Tiles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RMU.Yaku
+{
+    public class FullFlush : AbstractYaku
+    {
+        public FullFlush()
+        {
+            _name = "Full Flush";
+            _value = 6;
+            _getNameBehaviour = new StandardGetNameBehaviour();
+            _getValueBehaviour = new OpenDependentGetValueBehaviour();
+        }
+
+        public override bool CheckYaku(IHand hand, TileObject extraTile)
+        {
+            _hand = hand;
+            return false;
+        }
+    }
+}
