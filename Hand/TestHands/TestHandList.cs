5ff81ba2c908f61a9e2209945a8402856380eb72
Chad Scarborough <chadscarborough4@gmail.com>
2021-12-01 21:17:55 -0800

    Added static list of test hands for convnenience

---
+++ Hand/TestHands/TestHandList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RMU.Hand.TestHands
+{
+    public static class TestHandList
+    {
+        public static readonly TestHand ALL_SIMPLES_TEST_HAND = new AllSimplesTestHand();
+        public static readonly TestHand ALL_TERMINALS_TEST_HAND = new AllTerminalsTestHand();
+        public static readonly TestHand DRAGON_TEST_HAND = new DragonTestHand();
+        public static readonly TestHand HALF_FLUSH_TEST_HAND = new HalfFlushTestHand();
+        public static readonly TestHand NINE_GATES_TEST_HAND = new NineGatesTestHand();
+        public static readonly TestHand OPEN_TEST_HAND = new OpenTestHand();
+    }
+}
