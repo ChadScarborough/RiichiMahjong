de02c0e420fbe29a6e610d8d1d459e1cb5033d0b
Chad Scarborough <chadscarborough4@gmail.com>
2021-12-01 21:22:52 -0800

    Reorganized class; no substantial change

--- Yaku/YakuLists/YakuList.cs
+++ Yaku/YakuLists/YakuList.cs
@@ -6,6 +6,14 @@ namespace RMU.Yaku.YakuLists
 {
     public static class YakuList
     {
+        public static readonly AbstractYaku ALL_SIMPLES = new AllSimples();
+        public static readonly AbstractYaku ALL_TERMINALS_AND_HONORS = new AllTerminalsAndHonors();
+        public static readonly AbstractYaku FULL_FLUSH = new FullFlush();
+        public static readonly AbstractYaku GREEN_DRAGON = new GreenDragon();
+        public static readonly AbstractYaku HALF_FLUSH = new HalfFlush();
+        public static readonly AbstractYaku RED_DRAGON = new RedDragon();
+        public static readonly AbstractYaku WHITE_DRAGON = new WhiteDragon();
+
         private static List<AbstractYaku> _yakuList = new List<AbstractYaku>
         {
             ALL_SIMPLES,
@@ -21,13 +29,5 @@ namespace RMU.Yaku.YakuLists
         {
             return _yakuList;
         }
-
-        public static readonly AbstractYaku ALL_SIMPLES = new AllSimples();
-        public static readonly AbstractYaku ALL_TERMINALS_AND_HONORS = new AllTerminalsAndHonors();
-        public static readonly AbstractYaku FULL_FLUSH = new FullFlush();
-        public static readonly AbstractYaku GREEN_DRAGON = new GreenDragon();
-        public static readonly AbstractYaku HALF_FLUSH = new HalfFlush();
-        public static readonly AbstractYaku RED_DRAGON = new RedDragon();
-        public static readonly AbstractYaku WHITE_DRAGON = new WhiteDragon();
     }
 }
