8d3dcbd9bea2342fd817adc9d5810396369da6b0
Chad Scarborough <chadscarborough4@gmail.com>
2021-12-13 14:34:54 -0800

    Implemented All Greens

--- Yaku/YakuLists/YakumanList.cs
+++ Yaku/YakuLists/YakumanList.cs
@@ -5,8 +5,17 @@ namespace RMU.Yaku.YakuLists
 {
     public static class YakumanList
     {
+        public static readonly AbstractYakuman ALL_GREENS = new AllGreens();
+        public static readonly AbstractYakuman ALL_HONORS = new AllHonors();
+        public static readonly AbstractYakuman ALL_TERMINALS = new AllTerminals();
+        public static readonly AbstractYakuman BIG_THREE_DRAGONS = new BigThreeDragons();
+        public static readonly AbstractYakuman FOUR_BIG_WINDS = new FourBigWinds();
+        public static readonly AbstractYakuman THIRTEEN_ORPHANS = new ThirteenOrphans();
+        public static readonly AbstractYakuman THIRTEEN_WAIT_THIRTEEN_ORPHANS = new ThirteenWaitThirteenOrphans();
+
         private static List<AbstractYakuman> _yakumanList = new List<AbstractYakuman>
         {
+            ALL_GREENS,
             ALL_HONORS,
             ALL_TERMINALS,
             BIG_THREE_DRAGONS,
@@ -19,12 +28,5 @@ namespace RMU.Yaku.YakuLists
         {
             return _yakumanList;
         }
-
-        public static readonly AbstractYakuman ALL_HONORS = new AllHonors();
-        public static readonly AbstractYakuman ALL_TERMINALS = new AllTerminals();
-        public static readonly AbstractYakuman BIG_THREE_DRAGONS = new BigThreeDragons();
-        public static readonly AbstractYakuman FOUR_BIG_WINDS = new FourBigWinds();
-        public static readonly AbstractYakuman THIRTEEN_ORPHANS = new ThirteenOrphans();
-        public static readonly AbstractYakuman THIRTEEN_WAIT_THIRTEEN_ORPHANS = new ThirteenWaitThirteenOrphans();
     }
 }
