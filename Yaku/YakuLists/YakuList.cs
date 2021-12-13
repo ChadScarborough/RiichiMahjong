996f32e899a5e69798576cb2c108d1ab4d6b6ba4
Chad Scarborough <chadscarborough4@gmail.com>
2021-12-13 14:35:08 -0800

    Implemented All Greens

--- Yaku/YakuLists/YakuList.cs
+++ Yaku/YakuLists/YakuList.cs
@@ -9,9 +9,10 @@ namespace RMU.Yaku.YakuLists
         public static readonly AbstractYaku FULL_FLUSH = new FullFlush();
         public static readonly AbstractYaku GREEN_DRAGON = new GreenDragon();
         public static readonly AbstractYaku HALF_FLUSH = new HalfFlush();
+        public static readonly AbstractYaku LITTLE_THREE_DRAGONS = new LittleThreeDragons();
         public static readonly AbstractYaku RED_DRAGON = new RedDragon();
         public static readonly AbstractYaku WHITE_DRAGON = new WhiteDragon();
-        public static readonly AbstractYaku LITTLE_THREE_DRAGONS = new LittleThreeDragons();
+
 
         private static List<AbstractYaku> _yakuList = new List<AbstractYaku>
         {
---
+++ Yaku/Yakuman/AllGreens.cs
@@ -0,0 +1,68 @@
+﻿using RMU.Hand;
+using RMU.Tiles;
+using RMU.Globals;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RMU.Yaku.Yakuman
+{
+    public class AllGreens : AbstractYakuman
+    {
+        List<TileObject> _handTiles;
+        List<TileObject> _greenTileList = new List<TileObject>
+        {
+            StandardTileList.TwoSou(),
+            StandardTileList.ThreeSou(),
+            StandardTileList.FourSou(),
+            StandardTileList.SixSou(),
+            StandardTileList.EightSou(),
+            StandardTileList.GreenDragon()
+        };
+
+        public AllGreens()
+        {
+            _name = "All Greens";
+            _value = 13;
+            _getNameBehaviour = new StandardGetNameBehaviour();
+            _getValueBehaviour = new StandardGetValueBehaviour();
+        }
+
+        public override bool CheckYaku(IHand hand, TileObject extraTile)
+        {
+            InitializeHand(hand, extraTile);
+            return AllTilesInHandAreGreen();
+        }
+
+        private void InitializeHand(IHand hand, TileObject extraTile)
+        {
+            _handTiles = hand.GetAllTiles(extraTile);
+        }
+
+        private bool AllTilesInHandAreGreen()
+        {
+            foreach (TileObject tile in _handTiles)
+            {
+                if (TileIsNotGreen(tile))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private bool TileIsNotGreen(TileObject tile)
+        {
+            return ListContainsTile(tile, _greenTileList) == false;
+        }
+
+        private bool ListContainsTile(TileObject tile, List<TileObject> list)
+        {
+            foreach(TileObject listTile in list)
+            {
+                if (Functions.AreTilesEquivalent(tile, listTile)) return true;
+            }
+            return false;
+        }
+    }
+}
