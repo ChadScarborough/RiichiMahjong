7046a37e9e721e2173957149db97987891a4ca1a
ChadScarborough <chad.d.scarborough@gmail.com>
2022-09-20 22:04:53 -0700

    Add new test hands

---
+++ Hands/TestHands/TwoGroupsThreeIncompleteSequencesTestHand.cs
@@ -0,0 +1,20 @@
+﻿using RMU.Tiles;
+
+namespace RMU.Hands.TestHands;
+
+public sealed class TwoGroupsThreeIncompleteSequencesTestHand : TestHand
+{
+    public TwoGroupsThreeIncompleteSequencesTestHand()
+    {
+        _closedTiles = new List<Tile>
+        {
+            SixMan(), SevenMan(),
+            OnePin(), ThreePin(),
+            FivePin(), FivePin(), FivePin(),
+            OneSou(), TwoSou(),
+            SouthWind(), SouthWind(), SouthWind(),
+            WestWind()
+        };
+        // Should have shanten of 2
+    }
+}
