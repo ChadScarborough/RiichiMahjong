4857504f70a8c1be89edebe2e602ed31ffd72f95
ChadScarborough <chad.d.scarborough@gmail.com>
2022-09-20 22:05:06 -0700

    Improve shanten calculation

--- Shanten/ShantenFormulas.cs
+++ Shanten/ShantenFormulas.cs
@@ -5,12 +5,14 @@ public static class ShantenFormulas
     public static int CalculateStandardShanten(int groups, int pairs, int taatsu)
     {
         int standardShanten = 8 - ((2 * groups) + pairs + taatsu);
-        standardShanten = standardShanten <= 0 && pairs == 0 && taatsu >= 1 ? standardShanten + 1 : standardShanten;
+        //standardShanten = standardShanten <= 0 && pairs == 0 && taatsu >= 1 ? standardShanten + 1 : standardShanten;
+        if (taatsu >= 2 && pairs == 0) standardShanten++;
+
         return standardShanten <= 1
             ? (groups, pairs, taatsu) switch
             {
                 (4, 1, 0) => -1,
-                (4, 0, >= 0) or (3, >= 1, >= 1) or (3, >= 0, >= 2) => 0,
+                (4, 0, >= 0) or (3, >= 1, >= 1) or (3, >= 2, >= 0) => 0,
                 _ => 1
             }
             : standardShanten;
